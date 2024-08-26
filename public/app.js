ad5c02def90fedbf97a62d279700673b33fefbf7
urluur <luka.urs@icloud.com>
2024-08-26 17:37:36 +0200

    fixed #2 local server
    local server fetches localhost

--- public/app.js
+++ public/app.js
@@ -23,79 +23,90 @@ function getDepartures() {
   document.getElementById("results").hidden = true;
   document.getElementById("search").disabled = true;
 
-  axios.get('https://arriva-scraper.onrender.com/scrape?departure=' + departure + '&destination=' + arrival)
+  axios.get('http://localhost:3030/scrape?departure=' + departure + '&destination=' + arrival)
     .then(response => response.data)
     .then(data => {
-      console.log(data);
-      const departures = data.depart;
-      const returns = data.return;
-
-      if (departures.length == 0) {
-        alert("No departures found");
-        document.getElementById("loading").hidden = true;
-        document.getElementById("results").hidden = true;
-        document.getElementById("search").disabled = false;
-        return;
-      }
-
-      const departuresTable = document.getElementById("departures_table");
-      const returnsTable = document.getElementById("returns_table");
-
-      departuresTable.innerHTML = "";
-      returnsTable.innerHTML = "";
-
-      departures.forEach(departure => {
-        const row = document.createElement("tr");
-        row.onclick = () => {
-
-          document.querySelectorAll('#departures_table tr').forEach((tableRow) => {
-            tableRow.classList.remove('table-active');
-          });
-          row.classList.add('table-active');
-
-          returnsTable.innerHTML = "";
-          returns.forEach(returnItem => {
-            console.log(returnItem.departure + " " + departure.arrival);
-            if (returnItem.departure > departure.arrival) {
-              let row_tr = document.createElement("tr");
-              let departure_td = document.createElement("td");
-              let arrival_td = document.createElement("td");
-              let stay = document.createElement("td");
-
-              stay.innerHTML = betweenTimes(departure.arrival, returnItem.departure);
-              departure_td.innerHTML = returnItem.departure;
-              arrival_td.innerHTML = returnItem.arrival;
-
-              row_tr.appendChild(stay);
-              row_tr.appendChild(departure_td);
-              row_tr.appendChild(arrival_td);
-
-              returnsTable.appendChild(row_tr);
-            }
-          });
-        };
-
-        const departureCell = document.createElement("td");
-        const arrivalCell = document.createElement("td");
-
-        departureCell.innerHTML = departure.departure;
-        arrivalCell.innerHTML = departure.arrival;
-
-        row.appendChild(departureCell);
-        row.appendChild(arrivalCell);
-
-        departuresTable.appendChild(row);
-        document.getElementById("loading").hidden = true;
-        document.getElementById("results").hidden = false;
-        document.getElementById("search").disabled = false;
-
-      });
+      updateSite(data);
     })
-    .catch(error => {
-      console.log(error);
-      document.getElementById("loading").hidden = true;
-      document.getElementById("results").hidden = true;
-      document.getElementById("search").disabled = false;
+    .catch(() => { // looks like we are on render
+      axios.get('https://arriva-scraper.onrender.com/scrape?departure=' + departure + '&destination=' + arrival)
+        .then(response => response.json())
+        .then(data => {
+          updateSite(data);
+        })
+        .catch(error => {
+          console.log(error);
 
+          document.getElementById("loading").hidden = true;
+          document.getElementById("results").hidden = true;
+          document.getElementById("search").disabled = false;
+        });
     });
+}
+
+function updateSite(data) {
+  console.log(data);
+  const departures = data.depart;
+  const returns = data.return;
+
+  if (departures.length == 0) {
+    alert("No departures found");
+    document.getElementById("loading").hidden = true;
+    document.getElementById("results").hidden = true;
+    document.getElementById("search").disabled = false;
+    return;
+  }
+
+  const departuresTable = document.getElementById("departures_table");
+  const returnsTable = document.getElementById("returns_table");
+
+  departuresTable.innerHTML = "";
+  returnsTable.innerHTML = "";
+
+  departures.forEach(departure => {
+    const row = document.createElement("tr");
+    row.onclick = () => {
+
+      document.querySelectorAll('#departures_table tr').forEach((tableRow) => {
+        tableRow.classList.remove('table-active');
+      });
+      row.classList.add('table-active');
+
+      returnsTable.innerHTML = "";
+      returns.forEach(returnItem => {
+        console.log(returnItem.departure + " " + departure.arrival);
+        if (returnItem.departure > departure.arrival) {
+          let row_tr = document.createElement("tr");
+          let departure_td = document.createElement("td");
+          let arrival_td = document.createElement("td");
+          let stay = document.createElement("td");
+
+          stay.innerHTML = betweenTimes(departure.arrival, returnItem.departure);
+          departure_td.innerHTML = returnItem.departure;
+          arrival_td.innerHTML = returnItem.arrival;
+
+          row_tr.appendChild(stay);
+          row_tr.appendChild(departure_td);
+          row_tr.appendChild(arrival_td);
+
+          returnsTable.appendChild(row_tr);
+        }
+      });
+    };
+
+    const departureCell = document.createElement("td");
+    const arrivalCell = document.createElement("td");
+
+    departureCell.innerHTML = departure.departure;
+    arrivalCell.innerHTML = departure.arrival;
+
+    row.appendChild(departureCell);
+    row.appendChild(arrivalCell);
+
+    departuresTable.appendChild(row);
+    document.getElementById("loading").hidden = true;
+    document.getElementById("results").hidden = false;
+    document.getElementById("search").disabled = false;
+
+  });
 }
